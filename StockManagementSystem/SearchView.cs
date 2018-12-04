2a5f409a12e7b1afaef275a1d355110c728876b9
solayman35 <solayman.bubt@gmail.com>
2018-12-04 21:12:21 +0600

    first commit

---
+++ StockManagementSystem/SearchView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockManagementSystem
+{
+    class SearchView
+    {
+
+        public string ItemName { set; get; }
+        public string CategoryName { set; get; }
+        public string CompanyName { set; get; }
+        public int AvQuantity { set; get; }
+        public int ReorderLevel { set; get; }
+
+    }
+}
